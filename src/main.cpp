6b11173059065ff5602f7340647c2fd82369e543
VY <amzingfood@duck.com>
2023-03-20 13:47:28 -0700

    a

--- src/main.cpp
+++ src/main.cpp
@@ -544,7 +544,6 @@ int main() {
   Competition.drivercontrol(usercontrol);
   // Run the pre-autonomous function.
   pre_auton();
-
   // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
